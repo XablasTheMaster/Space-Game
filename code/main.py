1ecace03112aa89dd37669f83e9f7a0b720d292f
XablasTheMaster <n1c0l451lv4.32@gmail.com>
2026-01-28 21:49:08 -0300

    added main loop and organized some folders
    nice

---
+++ code/main.py
@@ -0,0 +1,26 @@
+import pygame as pg
+
+# pg setup
+pg.init()
+screen = pg.display.set_mode((1280, 720))
+clock = pg.time.Clock()
+running = True
+
+while running:
+    # poll for events
+    # pg.QUIT event means the user clicked X to close your window
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("red")
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pg.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pg.quit()
