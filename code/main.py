797848d47da9434255ba5a01203dd3477379d8a9
XablasTheMaster <n1c0l451lv4.32@gmail.com>
2026-02-01 11:46:28 -0300

    made the player bounce in the wall, added meteor sprite and laser sprite

--- code/main.py
+++ code/main.py
@@ -16,11 +16,24 @@ player_path = join("images", "player.png")
 player_surf = pg.image.load(player_path).convert_alpha()
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH * .5,WINDOW_HEIGHT * .5))
 player_pos = [100, 150]
+spd = .2
 
 # stars
 star_path = join("images", "star.png")
 star_surf = pg.image.load(star_path).convert_alpha()
 stars = []
+
+# meteor
+meteor_path = join("images", "meteor.png")
+meteor_surf = pg.image.load(meteor_path).convert_alpha()
+meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH * .5,WINDOW_HEIGHT * .5))
+
+# laser
+laser_path = join("images", "laser.png")
+laser_surf = pg.image.load(laser_path).convert_alpha()
+laser_padding = 20
+laser_rect = laser_surf.get_frect(bottomright = (WINDOW_WIDTH - laser_padding, WINDOW_HEIGHT - laser_padding))
+
 
 for i in range(20):
     x = randint(0, WINDOW_WIDTH-50)
@@ -33,9 +46,17 @@ while is_running:
             is_running = False
 
     screen.fill("darkgray")
-    player_rect.left += .2
-    screen.blit(player_surf, player_rect)
 
+    if player_rect.right > WINDOW_WIDTH:
+        spd *= -1
+    elif player_rect.left < 0:
+        spd *= -1
+
+    player_rect.left += spd 
+
+    screen.blit(player_surf, player_rect)
+    screen.blit(meteor_surf, meteor_rect)
+    screen.blit(laser_surf, laser_rect)
     for i in range(len(stars)):
         screen.blit(star_surf, stars[i])
 
